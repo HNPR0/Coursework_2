b1974ca11b0480588cd367c2410bae5df6a37216
Wirtz1 <oy2300530@tkh.edu.eg>
2025-11-15 13:31:55 +0200

    Ldr driver

---
+++ code/ldr.h
@@ -0,0 +1,15 @@
+#ifndef MAC_TYPES_H_
+#define MAC_TYPES_H_
+
+#include "macros_types.h"
+
+#endif /* MAC_TYPES_H_ */
+
+#ifndef LDR_H_
+#define LDR_H_
+
+void ldr_init(void);
+uint16 ldr_get_light_intensity(void);
+#define LDR_ANALOGUE_CHANNEL 2
+
+#endif  /* LDR_H_ */
